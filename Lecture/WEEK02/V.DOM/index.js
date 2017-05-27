d281ad8229911812937675d7331171500c68befd
Jee Hoon <yamoo9@naver.com>
2017-05-27 18:04:29 +0900

    TodoList 제거 기능 추가

--- Lecture/WEEK02/V.DOM/index.js
+++ Lecture/WEEK02/V.DOM/index.js
@@ -18,7 +18,7 @@
   // body.appendChild(p_node);
   body.insertAdjacentElement('afterbegin', p_node);
 
-}(window));
+}); // (window)
 
 // ------------------------------------
 // Virtual DOM 프로세스
@@ -30,20 +30,94 @@
 // 손쉽게 Budo 라는 개발 모듈 사용.
 var h             = require('virtual-dom/h');
 var createElement = require('virtual-dom/create-element');
+var diff          = require('virtual-dom/diff');
+var patch         = require('virtual-dom/patch');
 // console.log( typeof h ); // function
 // console.log( typeof createElement ); // function
 
+// 데이터 변경을 시뮬레이션 하기 위해 카운트 변수 정의
+// var count = 0; // 1, 2, 3,  .....
+
+// 배열 데이터를 가지고 실습
+var todoList = [
+  '일어나서 이불개기',
+  '아침 밥 먹기',
+  '늦지 않게 출근',
+  '열심히 버닝하여 일하기'
+];
+
+window.todoList = todoList;
+
+// h(selector, prop_map({}), [text or h()])
+// .container
+  // h1.todo-title  TodoList
+  // ul.todolist
+    // li.tidolist-item   {데이터 순환 처리}
+
+// todoList를 처리하기 위한 render() 함수 정의
+function render() {
+  var headline = h('h1.todo-headline', 'To Do List');
+  var listItems = todoList.map(function(item, index) {
+    return h('li.todolist-item', [
+      item,
+      h('button.todo-item-delete',{
+        type: 'button',
+        onclick: function(e){
+          // todoList 모델 데이터 수정(제거)
+          console.log('index:', index);
+          todoList.splice(index, 1);
+          console.log('todoList:', todoList);
+          // Virtual DOM의 diff() 함수가 변경 사항을 감지하여
+          // Patch 합니다. -> 사용자 화면 뷰 변경
+          update();
+        }
+      }, 'delete')
+    ]);
+  });
+  var list = h('ul.todolist', listItems);
+  var container = h('div.container', [headline, list]);
+  return container;
+}
+
+// render 함수 정의
+// function render(data) {
+//   var _vnode = h('p', {
+//     className: 'greeting'
+//   }, 'Hello Virtual DOM ' + data);
+//   // 제대로 VNode 생성되었느지 검증
+//   // console.log(vtree);
+//   return _vnode;
+// }
+
+// update() 함수 정의
+function update() {
+  // 이전 V_DOM === vtree
+  // 현재 V_DOM 생성 <= h(), render() 함수 재사용
+  // var newVtree = render(++count);
+  var newVtree = render();
+  // 이전 V_DOM, 현재 V_DOM 비교(diff)
+  var patches = diff(vtree, newVtree);
+  // 비교 결과 변경사항이 존재하면 패치(patch)
+  rootNode = patch(rootNode, patches);
+  // 현재 DOM을 이전 DOM으로 교체(변경)
+  vtree = newVtree;
+}
+
 // h() 함수를 사용하여 가상 DOM 트리(VTree) 작성 후, 반환
-var vtree = h('p', {
-  className: 'greeting'
-}, 'Hello Virtual DOM');
-// 제대로 VNode 생성되었느지 검증
-// console.log(vtree);
+// var vtree = render(count);
+var vtree = render();
 
 // DOM Node 생성 반환
 var rootNode = createElement(vtree);
 // 생성된 DOM Node 검증
-console.log(rootNode);
+// console.log(rootNode);
 
 // 실제 DOM에 반영
 document.body.appendChild(rootNode);
+
+// 시간이 지남에 따라 update() 함수를 실행
+// window.setInterval(function(){
+//   // 2초 마다 업데이트 수행
+//   update();
+//   // console.log(++count);
+// }, 2000);
