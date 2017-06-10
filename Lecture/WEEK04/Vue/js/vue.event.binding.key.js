f2a058c85305b0fab42445cae5248e9de919a91d
Jee Hoon <yamoo9@naver.com>
2017-06-10 17:30:44 +0900

    키보드 키 수식어

--- Lecture/WEEK04/Vue/js/vue.event.binding.key.js
+++ Lecture/WEEK04/Vue/js/vue.event.binding.key.js
@@ -5,10 +5,27 @@
   new Vue({
     el: '.demo',
     data: {
-
+      query: '',
+      directory: '파스타 피자 리조또 콜라 사이다'.split(' ')
     },
     methods: {
-
+      addInputItem: function(e) {
+        var t = e.target;
+        var v = t.value.trim();
+        if ( v === '' ) { return; }
+        this.directory.push(v);
+        t.value = '';
+      },
+      addItem: function(e) {
+        var t = e.target.localName === 'input' ? e.target : e.target.previousElementSibling;
+        var v = t.value.trim();
+        if ( v === '' ) {return;}
+        this.directory.push(v);
+        t.value = '';
+      },
+      instantSearch: function(e){
+        this.query = e.target.value;
+      }
     }
   });
 
