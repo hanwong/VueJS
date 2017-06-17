c83b21d6083f7b3297a84654269ef5e98a9a0af1
Jee Hoon <yamoo9@naver.com>
2017-06-17 17:32:12 +0900

    가져온 데이터를 다시금 정리

--- Lecture/WEEK05/Vue.filtering.sort.table/js/vue-filtering.sort.table.js
+++ Lecture/WEEK05/Vue.filtering.sort.table/js/vue-filtering.sort.table.js
@@ -5,7 +5,28 @@
   new Vue({
     el: '.filter-sort-table-demo',
     data: {
-
+      people: people,
+      search: '',
+      people_heading: 'picture name gender email phone'.split(' '),
+      people_contents: []
+    },
+    created: function() {
+      this.people = this.people.map(function(person){
+        var name = person.name;
+        person.name.full = name.first + ' ' + name.last;
+        return person;
+      });
+    },
+    mounted () {
+      var contents = this.people_contents;
+      var headings = this.people_heading;
+      this.people.forEach(function(person){
+        var content = {};
+        headings.forEach(function(item){
+          content[item] = person[item];
+        });
+        contents.push(content);
+      });
     }
   });
 
