9949c0a97306bb20c503543fe835f4b190a13d58
Jee Hoon <yamoo9@naver.com>
2017-06-17 16:30:59 +0900

    todoList 완료

--- Lecture/WEEK05/Vue.todolist/js/vue.todolist.js
+++ Lecture/WEEK05/Vue.todolist/js/vue.todolist.js
@@ -11,7 +11,30 @@
         { desc: 'jQuery 플러그인 패턴 집필', complete: false },
         { desc: 'FDS 스쿨 과정 포트폴리오 컨펌', complete: false },
         { desc: 'Vue 캠프 수업 준비', complete: true },
-      ]
+      ],
+      todo: ''
+    },
+    computed: {
+      completeList: function() {
+        return this.doList.filter(function(item){
+          return item.complete;
+        });
+      }
+    },
+    methods: {
+      changeLabelState: function(item) {
+        return { 'text-decoration': item.complete ? 'line-through' : '' };
+      },
+      addTodoItem: function() {
+        var value = this.todo.trim();
+        if ( value !== '' ) {
+          this.doList.unshift({
+            complete: false,
+            desc: value
+          });
+        }
+        this.todo = '';
+      }
     }
   });
 
