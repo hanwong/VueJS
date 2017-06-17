95b3fed2b5f53f924973869d47eb3cc6f90bca57
Jee Hoon <yamoo9@naver.com>
2017-06-17 15:50:10 +0900

    ToDo List 코드 추가

--- Lecture/WEEK05/Vue.todolist/js/vue.todolist.js
+++ Lecture/WEEK05/Vue.todolist/js/vue.todolist.js
@@ -2,6 +2,17 @@
 ;(function(global, Vue){
   'use strict';
 
-
+  global.todolist = new Vue({
+    el: '.demo-todolist',
+    data: {
+      doList: [
+        { desc: 'Vue.js 도서 쓰기', complete: false },
+        { desc: 'Vuex 패턴 연구', complete: true },
+        { desc: 'jQuery 플러그인 패턴 집필', complete: false },
+        { desc: 'FDS 스쿨 과정 포트폴리오 컨펌', complete: false },
+        { desc: 'Vue 캠프 수업 준비', complete: true },
+      ]
+    }
+  });
 
 })(window, window.Vue);
