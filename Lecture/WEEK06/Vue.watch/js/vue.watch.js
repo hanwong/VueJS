d1fabdb758841ceaa76af5c6d5171a608eda8f62
Jee Hoon <yamoo9@naver.com>
2017-06-24 15:18:11 +0900

    watch 를 중단하는 방법

--- Lecture/WEEK06/Vue.watch/js/vue.watch.js
+++ Lecture/WEEK06/Vue.watch/js/vue.watch.js
@@ -2,16 +2,37 @@
 ;(function(global, Vue){
   'use strict';
 
-  new Vue({
+  var unWatchQuery = null;
+
+  var app = new Vue({
     el: '.demo',
     data: {
       headline: 'Vue Watch Property',
       query: '입력을 기다리고 있습니다.', // 사용자 입력 초기 값
       watched_query: '' // 관찰할 속성의 기본 값
     },
+    // Life Cycle Hook
+    mounted: function() {
+      // Vue {} 인스턴스 외부에서 watch를 수행할 경우
+      // Vue 인스턴스 메서드 $watch() 활용
+      // 1. 라이프 사이클 훅 내에서 처리 가능
+      // 2. Vue{} 참조 변수를 통해 외부에서 가능
+      var vm = this;
+      unWatchQuery = vm.$watch('query', function(value, old_value) {
+        console.log('new: %s, old: %s', value, old_value);
+        global.setTimeout(function() {
+          vm.watched_query = '관찰 속성: ' + value;
+        }, 1000);
+      });
+    },
     methods: {
       assignQuery: function(e) {
         this.query = e.target.value;
+      },
+      // query 속성 관찰(watch)을 멈출 수 있는 메서드
+      unWatchQuery: function() {
+        unWatchQuery();
+        console.log('query 속성 관찰 중지되었습니다.');
       }
     },
     // 계산된 속성
@@ -33,16 +54,20 @@
     // Server 통신 처리 결과를 수행할 때 적합
     watch: {
       // computed 속성과 달리 watch 속성은 data 속성과 동일해도 된다.
-      query: function(value, old_value) {
-        var vm = this;
-        console.log('new: %s, old: %s', value, old_value);
-        // 지연된 처리 (서버 처리 시뮬레이션)
-        // 1초 뒤에 반응하여 작동
-        global.setTimeout(function() {
-          vm.watched_query = '관찰 속성: ' + value;
-        }, 1000);
-      }
+      // query: function(value, old_value) {
+      //   var vm = this;
+      //   console.log('new: %s, old: %s', value, old_value);
+      //   // 지연된 처리 (서버 처리 시뮬레이션)
+      //   // 1초 뒤에 반응하여 작동
+      //   global.setTimeout(function() {
+      //     vm.watched_query = '관찰 속성: ' + value;
+      //   }, 1000);
+      // }
     }
   });
+
+  // app // Vue {}
+  // app.query // data{ query }
+  // app.$watch('query', function(v, n){});
 
 })(window, window.Vue);
