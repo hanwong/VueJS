4bda52fa566d3281bfc0f72bc18f777497776e6a
Jee Hoon <yamoo9@naver.com>
2017-07-09 00:35:36 +0900

    Vue 필터 전역/지역 등록 형태로 변경

--- Lecture/WEEK08/Filter/js/main.js
+++ Lecture/WEEK08/Filter/js/main.js
@@ -2,20 +2,29 @@
 ;(function(global, Vue){
   'use strict';
 
-  Vue.filter('number', function(value){
+  // 비공개 함수
+  var changeCurrencyNumber = function(value){
     value = value.toString().split('').reverse();
     for ( var i=0, l=value.length; i<l; i+=3 ) {
       i > 0 && value.splice(i, 0, ',');
     }
     return value.reverse().join('');
-  });
+  };
 
-  Vue.filter('currency', function(value){
-    return value + '원';
-  });
+  // 전역 필터로 등록
+  Vue.filter('number', changeCurrencyNumber);
 
   new Vue({
-    el: '.app'
+    el: '.app',
+    // 지역 필터로 등록
+    filters: {
+      currency: function(value){
+        // 등록된 Vue.filter('number') 함수 활용
+        // value = Vue.filter('number')(value);
+        value = changeCurrencyNumber(value);
+        return value + '원';
+      }
+    }
   })
 
 })(window, window.Vue);
