e2079397b44f98892e161822fe68915a26861f7c
Jee Hoon <yamoo9@naver.com>
2017-07-15 15:20:30 +0900

    Vue 디렉티브

--- Lecture/WEEK09/Directive/js/vue.directive.js
+++ Lecture/WEEK09/Directive/js/vue.directive.js
@@ -3,10 +3,44 @@
   'use strict';
 
   // 글로벌 디렉티브 정의
+  Vue.directive('focus', {
+    bind: function(el, binding){
+      console.log(binding); // { name, rawName, modifiers, arg, ... }
+      // console.log(el); // <a>
+      // el.style.cssText = '\
+      //   color: tan;\
+      //   padding: 0.3em 1em;\
+      //   outline: 3px solid currentColor\
+      // ';
+      // el.focus();
+    },
+    inserted: function(el) {
+      console.log(el); // control element
+    },
+    update: function() {},
+    componentUpdated: function() {},
+    unbind: function() {}
+  });
+  // Vue.directive('focus', function(){});
+
+  // Directive RawName: 'v-marking'
+  // Directive Name:    'marking'
+  Vue.directive('marking', {
+    bind: function(el) {
+      el.style.background = '#ff0';
+    },
+    // Hook Functions
+    // bind, inserted, update, componentUpdated, unbind
+    unbind: function(el) {
+      console.log('unbind directive');
+    }
+  });
 
   global.vm = new Vue({
     el: '.app',
-    data: {}
+    data: {
+      is_visible_directive: true
+    }
   });
 
 })(window, window.Vue);
