1da424d87af3e9b19c37f2d6802e4976444d6195
Jee Hoon <yamoo9@naver.com>
2017-07-15 16:02:29 +0900

    Vue Directive Bind, UnBind, Update

--- Lecture/WEEK09/Directive/js/vue.directive.js
+++ Lecture/WEEK09/Directive/js/vue.directive.js
@@ -26,20 +26,61 @@
   // Directive RawName: 'v-marking'
   // Directive Name:    'marking'
   Vue.directive('marking', {
-    bind: function(el) {
-      el.style.background = '#ff0';
+    bind: function(el, o) {
+      var value = o.value || '#ff0';
+      var delay = o.modifiers.delay || 0;
+      if ( o.modifiers.animate ) {
+        el.style.transition = 'all 1s ease';
+      }
+      if ( o.modifiers.delay ) {
+        delay = 1000;
+      }
+      global.setTimeout(function(){
+        el.style[o.arg] = value;
+      }, delay);
+    },
+    update: function(el, binding, newV, oldV) {
+      console.log('update');
+      el.style[binding.arg] = 'maroon';
     },
     // Hook Functions
     // bind, inserted, update, componentUpdated, unbind
     unbind: function(el) {
       console.log('unbind directive');
+      global.document.body.style.height = '100vh';
+      global.document.body.style.transition = 'all 1s ease-out';
+      global.document.body.style.background = '#3273dc';
     }
   });
 
   global.vm = new Vue({
     el: '.app',
     data: {
-      is_visible_directive: true
+      is_visible_directive: true,
+      numbers: [
+        'coffee',
+        'cafe',
+        'mocha'
+      ]
+    },
+    components: {
+      mySpan: {
+        props: {
+          data: {
+            type: Array,
+            default: []
+          }
+        },
+        template: '\
+          <div>\
+            <span\
+              class="my-span"\
+              v-marking:color.animate="\'tan\'"\
+              @click="$emit(\'call\')"\
+              v-for="item in data"> {{item}} </span>\
+          </div>\
+        '
+      }
     }
   });
 
