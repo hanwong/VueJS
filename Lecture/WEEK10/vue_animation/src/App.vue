42f4156026ed6fa6a28738df7a8cbf83f34f3093
Jee Hoon <yamoo9@naver.com>
2017-07-22 14:51:07 +0900

    10주차 수업 첫시간 진행자료 추가

---
+++ Lecture/WEEK10/vue_animation/src/App.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  #app
+    h1 {{ name }}
+    app-nav(:contents="contents")
+</template>
+
+<script>
+// 컴포넌트 객체 불러오기
+import AppNav from './components/AppNav.vue';
+
+export default {
+  name: 'app',
+  // 로컬 컴포넌트 객체 설정
+  components: {
+    'app-nav': AppNav
+  },
+  // App 컴포넌트 데이터
+  data () {
+    return {
+      name: 'Vue.js Application using webpack',
+      contents: [
+        {
+          link: 'https://google.com', content:'Google'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+html
+  font-size: 100%
+  background: #fff
+body
+  margin: 0
+</style>
---
+++ Lecture/WEEK10/vue_animation/src/components/AppNav.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  nav.app-nav
+    ul
+      li
+        a(href="/") Home
+</template>
+
+<script>
+// 컴포넌트 객체 내보내기
+export default {
+  // 컴포넌트 이름 설정
+  name: 'AppNav',
+  // 부모 컴포넌트에서 전달된 속성
+  props: ['contents'],
+  // 라이프사이클 훅
+  created () {
+    console.log('AppNav Created');
+    console.log(this.contents[0].link);
+  },
+  mounted () {
+    console.log('AppNav Mounted');
+  },
+  // 컴포넌트 데이터
+  data() {
+    return  {}
+  }
+};
+</script>
